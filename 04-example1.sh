8e0261a6347cbf50b44ae80f7cf8cd61ac3d228f
Aruna676 <arunamallela676@gmail.com>
2023-12-29 23:15:59 +0530

    added 2nd script

--- 04-example1.sh
+++ 04-example1.sh
@@ -5,7 +5,7 @@ echo "please enter your username::"
 read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
 echo "please enter your password::"
-reas -s PASSWORD
+read -s PASSWORD
 
 echo "Username is :$USERNAME, Password id :$PASSWORD" # I am printing just for validation you should not print username and password in the script
 
