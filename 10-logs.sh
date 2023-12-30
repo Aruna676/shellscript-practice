f5944c96b9a19e4c5f39368f68b61c8c8c137779
Aruna676 <arunamallela676@gmail.com>
2023-12-30 14:31:59 +0530

    added 2nd script

--- 10-logs.sh
+++ 10-logs.sh
@@ -2,18 +2,22 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N=\e[0m"
 
-LOGFILE="/temp/$0-$TIMESTAMP.log"
+LOGFILE="/temp/$0-$R TIMESTAMP.log"
 
 
 VALIDATE()
 {
 if [ $? -ne 0 ]
 then
-echo "ERROR:: $2 ... FAILED"
+echo "ERROR:: $2 ...$R FAILED $N"
 exit 1 
 else
-echo "$2 ... SUCCESS"
+echo "$2 ...  $G SUCCESS$N"
 fi 
 }
 if [ $ID -ne 0 ]
